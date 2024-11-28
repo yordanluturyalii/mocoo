27c0c927b5590fa53cef47652e1e90b4ff49983a
yordanluturyalii <yordanluturyali@gmail.com>
2024-11-28 21:32:44 +0700

    Update navigation ui💄

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-neutral-900">
             <livewire:layout.navigation />
 
             <!-- Page Content -->
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Pages\HistoryPage;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::middleware(['auth'])->group(function () {
+    Route::view('dashboard', 'dashboard')
+        ->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+    Route::view('profile', 'profile')
+        ->name('profile');
+
+    Route::get('history', HistoryPage::class)
+        ->name('history');
+});
 
 require __DIR__.'/auth.php';
